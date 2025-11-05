110cdd901ec5488f69f5f1db26e29adc330c2af5
Arthur <0001135648@senaimgaluno.com.br>
2025-11-05 23:14:51 +0000

    Atualiza código fonte e configurações para GitHub Pages

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const vite_1 = require("vite");
+const plugin_react_1 = __importDefault(require("@vitejs/plugin-react"));
+exports.default = (0, vite_1.defineConfig)({
+    plugins: [(0, plugin_react_1.default)()],
+    base: "/Tarefa3/",
+    server: {
+        allowedHosts: ["fhgqf7-5173.csb.app"], // 👈 seu host do CodeSandbox
+    },
+});
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Tarefa3/",
   server: {
     allowedHosts: ["fhgqf7-5173.csb.app"], // 👈 seu host do CodeSandbox
   },
